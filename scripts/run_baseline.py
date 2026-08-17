ed82f4d70e99c2ed7bb0a9146baa111badf07b17
Viet Anh Ho <222012976+gmtigrisva123@users.noreply.github.com>
2026-08-17 09:11:28 +0700

    docs: added run_baseline file: done the task in day 1

---
+++ scripts/run_baseline.py
@@ -0,0 +1,18 @@
+import json
+from retriever.retrieve import retrieve
+from generator.generate import generate_answer
+from eval.scorer import evaluate
+data = [json.loads(l) for l in open("data/hotpotqa_dev_300.jsonl")]
+predictions, golds = [], []
+for record in data:
+    passages = retrieve(record["question"], k=5) 
+    answer = generate_answer(record["question"], passages) 
+    predictions.append(answer) 
+    golds.append(record["answer"])
+    results = evaluate(predictions, golds)
+    print(results)
+with open("results/baseline_week1.json", "w") as f:
+    json.dump({
+            "results": results, 
+            "predictions": predictions
+        }, f, indent=2)
